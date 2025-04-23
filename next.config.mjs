482a31d33e5aeea04f8c7d26976959637c941b85
Imam Hossen <imam.frontend404@gmail.com>
2025-04-23 22:55:59 +0600

    fa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        css: false, // Disable lightningcss
+      },
+};
 
 export default nextConfig;
